5498a10973a14c59c92712cd86f31fbea2ad914c
nvaro <varonono@yahoo.com>
2018-07-30 09:28:21 +0300

    angular-tour-of-heroes final 2.0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { HeroDetailComponent }  from './hero-detail/hero-detail.component';
 import { HeroesComponent }      from './heroes/heroes.component';
 //import { HeroSearchComponent }  from './hero-search/hero-search.component';
 import { MessagesComponent }    from './messages/messages.component';
+import { HeroSearchComponent } from './hero-search/hero-search.component';
 
 @NgModule({
   imports: [
@@ -34,7 +35,8 @@ import { MessagesComponent }    from './messages/messages.component';
     DashboardComponent,
     HeroesComponent,
     HeroDetailComponent,
-    MessagesComponent
+    MessagesComponent,
+    HeroSearchComponent
    // HeroSearchComponent
   ],
   bootstrap: [ AppComponent ]
